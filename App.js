95cdf40966a699aedc8e515ce4c4c770957aaf89
Yunho Kim <31121701+Kandreaop@users.noreply.github.com>
2022-03-05 10:04:31 +0900

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
+import {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, TextInputComponent, ScrollView} from 'react-native';
+import { theme } from './colors';
 
 export default function App() {
+  const[working, setWorking] = useState(true);
+  const[text, setText] = useState("");
+  const[toDos, setTodDos] = useState({});
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (payload) => setText(payload)
+  const addToDo = () => {
+    if(text === ""){
+      return;
+    }
+    const newToDos = {...toDos,
+   [Date.now()]: {text, work: working} };
+    setTodDos(newToDos);
+    setText("");
+  };
+  console.log(toDos);
+// 중괄호 두번 쓰고 ... 하면 모든 스타일을 가져올 수 있다.
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style= "dark"/>
+      <View style = {styles.header}>
+        <TouchableOpacity onPress={work}>
+          <Text style = {{...styles.button, color: working ? theme.black: theme.gray}}>Work</Text>  
+        </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+          <Text style = {{...styles.button, color: !working ? theme.black: theme.gray}}>Travel</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TextInput 
+        onSubmitEditing={addToDo}
+        onChangeText={onChangeText}
+        returnKeyType = "Done"
+        value={text}
+        placeholder={working ? "Add a To Do": "Where do you want to go"}
+        placeholderTextColor = "#DEDEDE"
+        style = {styles.input}/>
+        <ScrollView>{Object.keys(toDos).map(key=> <View style={styles.todo} key = {key}>
+          <Text>{toDos[key].text}</Text>
+        </View>)}</ScrollView>
+      </View>
     </View>
   );
 }
@@ -13,8 +52,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.background,
+    paddingHorizontal : 40,
   },
+  header :{
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop : 100,
+  },
+  button :{
+    fontSize : 30,
+    fontWeight : "600"
+  },
+
+  input :{
+    backgroundColor: "#808080",
+    color : "#DEDEDE",
+    fontWeight : "600",
+    paddingVertical : 15,
+    paddingHorizontal: 20,
+    borderRadius : 30,
+    marginTop : 20
+
+  }
 });
---
+++ colors.js
@@ -0,0 +1,5 @@
+export const theme = {
+    background:"#DEDEDE",
+    gray: "#7F7F7F",
+    black : "#292929",
+}
